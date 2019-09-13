8617aa08de186f43be29a8e0009916d8d85b3d45
kbysiec <kamil.bysiec.agileplayers@gmail.com>
2019-09-13 21:48:42 +0200

    Polishing extension commands

--- src/extension.ts
+++ src/extension.ts
@@ -2,14 +2,14 @@ import * as vscode from "vscode";
 import ExtensionController from "./ExtensionController";
 
 export async function activate(context: vscode.ExtensionContext) {
-  console.log('Extension "vscode-go-to-mdn" has been activated.');
+  console.log('Extension "Go to MDN" has been activated.');
 
   context.subscriptions.push(
-    vscode.commands.registerCommand("extension.goToMdn", async () => {
+    vscode.commands.registerCommand("goToMDN.browse", async () => {
       const controller = new ExtensionController(context);
       await controller.showQuickPick();
     }),
-    vscode.commands.registerCommand("extension.clearCache", async () => {
+    vscode.commands.registerCommand("goToMDN.clearCache", async () => {
       const controller = new ExtensionController(context);
       await controller.clearCache();
       vscode.window.showInformationMessage(
