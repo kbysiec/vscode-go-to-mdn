fc3ca02d1b228168842b21c015205c863a296ffb
kbysiec <kamil.bysiec.agileplayers@gmail.com>
2019-09-13 16:45:34 +0200

    Register palette commands for browsing and cache clearing

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,22 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import ExtensionController from "./ExtensionController";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
+  console.log('Extension "vscode-go-to-mdn" has been activated.');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-go-to-mdn" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.commands.registerCommand("extension.goToMdn", async () => {
+      const controller = new ExtensionController(context);
+      await controller.showQuickPick();
+    }),
+    vscode.commands.registerCommand("extension.clearCache", async () => {
+      const controller = new ExtensionController(context);
+      await controller.clearCache();
+      vscode.window.showInformationMessage(
+        "Go to MDN extension: cache cleared"
+      );
+    })
+  );
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
