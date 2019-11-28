ca848bff0c6e412e78eeb9ae3af3f43fc9ce77b0
kbysiec <kamil.bysiec.agileplayers@gmail.com>
2019-11-28 13:51:13 +0100

    Polishing of cache tests

--- src/test/tests/cache.test.ts
+++ src/test/tests/cache.test.ts
@@ -1,75 +1,74 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 import { assert } from "chai";
 import * as sinon from "sinon";
-import Cache from '../../cache';
+import Cache from "../../cache";
 import QuickPickExtendedItem from "../../interfaces/QuickPickExtendedItem";
 import Item from "../../interfaces/Item";
 import ItemType from "../../enums/ItemType";
 import { config } from "../../config";
+import * as mock from "../mocks/cache.mock";
 
-describe("Cache", function () {
+describe("Cache", function() {
   let cache: Cache;
   let context: vscode.ExtensionContext;
   let updateSpy: any;
 
-  before(function () {
+  before(function() {
     sinon.stub(config, "filesCacheKey").value("filesCache");
     sinon.stub(config, "cacheKey").value("cache");
-    sinon.stub(config, "rootUrl").value("https://api.github.com/repos/mdn/browser-compat-data/contents/README.md?ref=master");
+    sinon
+      .stub(config, "rootUrl")
+      .value(
+        "https://api.github.com/repos/mdn/browser-compat-data/contents/README.md?ref=master"
+      );
 
     updateSpy = sinon.spy();
 
     context = {
       subscriptions: [],
       workspaceState: {
-        get: () => { },
-        update: () => Promise.resolve(),
+        get: () => {},
+        update: () => Promise.resolve()
       },
       globalState: {
-        get: () => { },
-        update: updateSpy,
+        get: () => {},
+        update: updateSpy
       },
       extensionPath: "",
       storagePath: "",
       globalStoragePath: "",
       logPath: "",
-      asAbsolutePath: (relativePath: string) => relativePath,
+      asAbsolutePath: (relativePath: string) => relativePath
     };
-
 
     cache = new Cache(context);
   });
 
-  after(function () {
+  afterEach(function() {
     sinon.restore();
   });
 
-  describe("updateFlatFilesListCache", function () {
-    it("should function exist", function () {
-      const actual = typeof (cache.updateFlatFilesListCache);
+  describe("updateFlatFilesListCache", function() {
+    it("should function exist", function() {
+      const actual = typeof cache.updateFlatFilesListCache;
       const expected = "function";
       assert.equal(actual, expected);
     });
 
-    it("should update cache value", function () {
-
-      const data: Item[] = [
-        { name: "sub-label", url: "#", type: ItemType.File, breadcrumbs: ["api", "test-label", "sub-label"] },
-        { name: "sub-label 2", url: "https://sub-label-2.com", type: ItemType.File, breadcrumbs: ["api", "test-label", "sub-label 2"] }
-      ];
-
+    it("should update cache value", function() {
       let getStub = sinon.stub(context.globalState, "get").returns({});
+      const items: Item[] = mock.items;
       const get = context.globalState.get as any;
       const update = context.globalState.update as any;
 
-      cache.updateFlatFilesListCache(data);
+      cache.updateFlatFilesListCache(items);
       let getCalled = get.called;
 
       assert.equal(getCalled, true);
       getStub.restore();
 
       getStub = sinon.stub(context.globalState, "get").returns(undefined);
-      cache.updateFlatFilesListCache(data);
+      cache.updateFlatFilesListCache(items);
       getCalled = get.called;
 
       const updateCalled = update.calledTwice;
@@ -82,32 +81,28 @@ describe("Cache", function () {
     });
   });
 
-  describe("updateTreeItemsByUrlFromCache", function () {
-    it("should function exist", function () {
-      const actual = typeof (cache.updateTreeItemsByUrlFromCache);
+  describe("updateTreeItemsByUrlFromCache", function() {
+    it("should function exist", function() {
+      const actual = typeof cache.updateTreeItemsByUrlFromCache;
       const expected = "function";
       assert.equal(actual, expected);
     });
 
-    it("should update cache value if item is undefined", function () {
-
-      const data: Item[] = [
-        { name: "sub-label", url: "#", type: ItemType.File, breadcrumbs: ["api", "test-label", "sub-label"] },
-        { name: "sub-label 2", url: "https://sub-label-2.com", type: ItemType.File, breadcrumbs: ["api", "test-label", "sub-label 2"] }
-      ];
+    it("should update cache value if item is undefined", function() {
+      const items: Item[] = mock.items;
 
       let getStub = sinon.stub(context.globalState, "get").returns({});
       const get = context.globalState.get as any;
       const update = context.globalState.update as any;
 
-      cache.updateTreeItemsByUrlFromCache(data);
+      cache.updateTreeItemsByUrlFromCache(items);
       let getCalled = get.called;
 
       assert.equal(getCalled, true);
       getStub.restore();
 
       getStub = sinon.stub(context.globalState, "get").returns(undefined);
-      cache.updateTreeItemsByUrlFromCache(data);
+      cache.updateTreeItemsByUrlFromCache(items);
       getCalled = get.called;
 
       const updateCalled = update.calledTwice;
@@ -119,27 +114,22 @@ describe("Cache", function () {
       updateSpy.resetHistory();
     });
 
-    it("should update cache value if item is passed", function () {
-
-      const data: Item[] = [
-        { name: "sub-label", url: "#", type: ItemType.File, breadcrumbs: ["api", "test-label", "sub-label"] },
-        { name: "sub-label 2", url: "https://sub-label-2.com", type: ItemType.File, breadcrumbs: ["api", "test-label", "sub-label 2"] }
-      ];
-
-      const item: Item = { name: "sub-label 3", url: "#", type: ItemType.File, breadcrumbs: ["api", "test-label", "sub-label 3"] };
+    it("should update cache value if item is passed", function() {
+      const items: Item[] = mock.items;
+      const item: Item = mock.item;
 
       let getStub = sinon.stub(context.globalState, "get").returns({});
       const get = context.globalState.get as any;
       const update = context.globalState.update as any;
 
-      cache.updateTreeItemsByUrlFromCache(data, item);
+      cache.updateTreeItemsByUrlFromCache(items, item);
       let getCalled = get.called;
 
       assert.equal(getCalled, true);
       getStub.restore();
 
       getStub = sinon.stub(context.globalState, "get").returns(undefined);
-      cache.updateTreeItemsByUrlFromCache(data, item);
+      cache.updateTreeItemsByUrlFromCache(items, item);
       getCalled = get.called;
 
       const updateCalled = update.calledTwice;
@@ -152,105 +142,82 @@ describe("Cache", function () {
     });
   });
 
-  describe("getFlatFilesFromCache", function () {
-    it("should function exist", function () {
-      const actual = typeof (cache.getFlatFilesFromCache);
+  describe("getFlatFilesFromCache", function() {
+    it("should function exist", function() {
+      const actual = typeof cache.getFlatFilesFromCache;
       const expected = "function";
       assert.equal(actual, expected);
     });
 
-    it("should return value from cache", function () {
-      const returnData: Item[] = [
-        { name: "sub-label", url: "#", type: ItemType.File, breadcrumbs: ["api", "test-label", "sub-label"] },
-        { name: "sub-label 2", url: "https://sub-label-2.com", type: ItemType.File, breadcrumbs: ["api", "test-label", "sub-label 2"] }
-      ];
-
+    it("should return value from cache", function() {
+      const items: Item[] = mock.items;
       let getStub = sinon.stub(context.globalState, "get").returns({
-        "files": returnData
+        files: items
       });
 
       const expected = cache.getFlatFilesFromCache();
-
-      assert.deepEqual(expected, returnData);
-
+      assert.deepEqual(expected, items);
       getStub.restore();
     });
 
-    it("should return empty array from cache", function () {
-      const returnData: Item[] = [];
-
+    it("should return empty array from cache", function() {
+      const items: Item[] = [];
       let getStub = sinon.stub(context.globalState, "get").returns(undefined);
+
       const expected = cache.getFlatFilesFromCache();
-
-      assert.deepEqual(expected, returnData);
-
+      assert.deepEqual(expected, items);
       getStub.restore();
     });
   });
 
-  describe("getTreeItemsByUrlFromCache", function () {
-    it("should function exist", function () {
-      const actual = typeof (cache.getTreeItemsByUrlFromCache);
+  describe("getTreeItemsByUrlFromCache", function() {
+    it("should function exist", function() {
+      const actual = typeof cache.getTreeItemsByUrlFromCache;
       const expected = "function";
       assert.equal(actual, expected);
     });
 
-    it("should return value from cache if item is undefined", function () {
-      const returnData: Item[] = [
-        { name: "sub-label", url: "#", type: ItemType.File, breadcrumbs: ["api", "test-label", "sub-label"] },
-        { name: "sub-label 2", url: "https://sub-label-2.com", type: ItemType.File, breadcrumbs: ["api", "test-label", "sub-label 2"] }
-      ];
-
+    it("should return value from cache if item is undefined", function() {
+      const items: Item[] = mock.items;
       let getStub = sinon.stub(context.globalState, "get").returns({
-        [config.rootUrl]: returnData
+        [config.rootUrl]: items
       });
 
       const expected = cache.getTreeItemsByUrlFromCache();
-
-      assert.deepEqual(expected, returnData);
-
+      assert.deepEqual(expected, items);
       getStub.restore();
     });
 
-    it("should return value from cache if item is passed", function () {
-      const qpItem: QuickPickExtendedItem = { label: "test-label", url: "#", type: ItemType.Directory, breadcrumbs: [] };
-
-      const returnData: Item[] = [
-        { name: "sub-label", url: "#", type: ItemType.File, breadcrumbs: ["api", "test-label", "sub-label"] },
-        { name: "sub-label 2", url: "https://sub-label-2.com", type: ItemType.File, breadcrumbs: ["api", "test-label", "sub-label 2"] }
-      ];
-
+    it("should return value from cache if item is passed", function() {
+      const qpItem: QuickPickExtendedItem = mock.qpItem;
+      const items: Item[] = mock.items;
       let getStub = sinon.stub(context.globalState, "get").returns({
-        [qpItem.url]: returnData
+        [qpItem.url]: items
       });
 
       const expected = cache.getTreeItemsByUrlFromCache(qpItem);
-
-      assert.deepEqual(expected, returnData);
-
+      assert.deepEqual(expected, items);
       getStub.restore();
     });
 
-    it("should return empty array from cache if key not found", function () {
-      const returnData: Item[] = [];
-
+    it("should return empty array from cache if key not found", function() {
+      const items: Item[] = [];
       let getStub = sinon.stub(context.globalState, "get").returns(undefined);
+
       const expected = cache.getTreeItemsByUrlFromCache();
-
-      assert.deepEqual(expected, returnData);
-
+      assert.deepEqual(expected, items);
       getStub.restore();
     });
   });
 
-  describe("clearCache", function () {
-    it("should function exist", function () {
-      const actual = typeof (cache.clearCache);
+  describe("clearCache", function() {
+    it("should function exist", function() {
+      const actual = typeof cache.clearCache;
       const expected = "function";
       assert.equal(actual, expected);
     });
 
-    it("should clear cache", function () {
+    it("should clear cache", function() {
       const update = context.globalState.update as any;
       cache.clearCache();
 
