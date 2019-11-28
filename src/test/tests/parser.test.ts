9d17eb727b648dd8dcab8ed301ae5308639d5d64
kbysiec <kamil.bysiec.agileplayers@gmail.com>
2019-11-28 13:40:46 +0100

    Polishing of parser tests

--- src/test/tests/parser.test.ts
+++ src/test/tests/parser.test.ts
@@ -1,11 +1,8 @@
-import * as vscode from "vscode";
 import { assert } from "chai";
-import * as sinon from "sinon";
 import Parser from "../../parser";
-import QuickPickExtendedItem from "../../interfaces/QuickPickExtendedItem";
 import Item from "../../interfaces/Item";
 import ItemType from "../../enums/ItemType";
-import { config } from "../../config";
+import * as mock from "../mocks/parser.mock";
 
 describe("Parser", function() {
   let parser: Parser;
@@ -22,27 +19,22 @@ describe("Parser", function() {
     });
 
     it("should return array of Item elements", function() {
-      const inputData: any = {
-        items: [
-          {
-            name: "sub-label",
-            url: "#",
-            breadcrumbs: ["api", "test-label", "sub-label"]
-          },
-          {
-            name: "sub-label 2",
-            url: "https://sub-label-2.com",
-            breadcrumbs: ["api", "test-label", "sub-label 2"]
-          }
-        ]
-      };
+      const inputData: any = mock.flatElementsInput;
 
       const actual = parser.parseFlatElements(inputData);
       const expected: Item[] = [
         {
           name: "sub-label",
           url: "#",
-          parent: undefined,
+          parent: {
+            name: "elements",
+            url:
+              "https://api.github.com/repos/mdn/browser-compat-data/contents/svg/elements?ref=master",
+            type: ItemType.Directory,
+            parent: undefined,
+            rootParent: undefined,
+            breadcrumbs: ["svg", "elements"]
+          },
           type: ItemType.File,
           breadcrumbs: ["api", "test-label", "sub-label"]
         },
@@ -54,14 +46,12 @@ describe("Parser", function() {
           breadcrumbs: ["api", "test-label", "sub-label 2"]
         }
       ];
-
       assert.deepEqual(actual, expected);
     });
 
     it("should return empty array", function() {
       const actual = parser.parseFlatElements({});
       const expected: Item[] = [];
-
       assert.deepEqual(actual, expected);
     });
   });
@@ -74,11 +64,7 @@ describe("Parser", function() {
     });
 
     it("should return array of Item elements once content contains has links", function() {
-      const content = `*Please note that we have not (yet) migrated all compatibility data from the MDN wiki pages into this repository.*
-      - [label/](https://github.com/mdn/browser-compat-data/tree/master/label) contains data for each Web API interface.
-      - [category/](https://github.com/mdn/browser-compat-data/tree/master/category) contains data for CSS properties, selectors, and at-rules.
-      [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML) elements, attributes, and global attributes.
-      ## Format of the browser compat json files`;
+      const content: string = mock.rootDirectoriesWithLinks;
 
       const actual = parser.parseRootDirectories(content);
       const expected: Item[] = [
@@ -97,52 +83,19 @@ describe("Parser", function() {
           breadcrumbs: ["category"]
         }
       ];
-
       assert.deepEqual(actual, expected);
     });
 
     it("should return array of Item elements once content doesn't contain any link", function() {
-      const content = `*Please note that we have not (yet) migrated all compatibility data from the MDN wiki pages into this repository.*
-      [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML) elements, attributes, and global attributes.
-      ## Format of the browser compat json files`;
+      const content: string = mock.rootDirectoriesNoLink;
 
       const actual = parser.parseRootDirectories(content);
       const expected: Item[] = [];
-
       assert.deepEqual(actual, expected);
     });
   });
 
   describe("parseElements", function() {
-    let content: string;
-    before(function() {
-      content = `{
-        "svg": {
-          "elements": {
-            "animateColor": {
-              "__compat": {
-                "mdn_url": "https://developer.mozilla.org/docs/Web/SVG/Element/animateColor",
-                "support": {
-                  "chrome": {
-                    "version_added": false
-                  }
-                }
-              },
-              "by": {
-                "__compat": {
-                  "support": {
-                    "chrome": {
-                      "version_added": false
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }`;
-    });
-
     it("should function exist", function() {
       const actual = typeof parser.parseElements;
       const expected = "function";
@@ -150,23 +103,8 @@ describe("Parser", function() {
     });
 
     it("should return array of Item elements if content is array of files without unnecessary nesting of keys", function() {
-      const item: Item = {
-        name: "animate Color",
-        url:
-          "https://api.github.com/repos/mdn/browser-compat-data/contents/svg/elements/animateColor.json?ref=master",
-        type: ItemType.Directory,
-        parent: {
-          name: "elements",
-          url:
-            "https://api.github.com/repos/mdn/browser-compat-data/contents/svg/elements?ref=master",
-          type: ItemType.Directory,
-          parent: undefined,
-          rootParent: undefined,
-          breadcrumbs: ["svg", "elements"]
-        },
-        rootParent: undefined,
-        breadcrumbs: ["svg", "elements", "animate Color"]
-      };
+      const content: string = mock.parseElementsContent;
+      const item: Item = mock.parseElementsItem;
 
       const actual = parser.parseElements(content, item);
       const expected: Item[] = [
@@ -191,52 +129,8 @@ describe("Parser", function() {
     });
 
     it("should return array of Item elements if content is array of files with unnecessary nesting of keys", function() {
-      content = `
-      {
-        "webdriver": {
-          "commands": {
-            "AcceptAlert": {
-              "__compat": {
-                "mdn_url": "https://developer.mozilla.org/docs/Web/WebDriver/Commands/AcceptAlert",
-                "support": {
-                  "chrome": {
-                    "version_added": "65"
-                  }
-                },
-                "status": {
-                  "experimental": false
-                }
-              },
-              "scheme": {
-                "wildcard": {
-                  "__compat": {
-                    "description": "Wildcard <code>*</code> scheme"
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-      `;
-
-      const item: Item = {
-        name: "Accept Alert",
-        url:
-          "https://api.github.com/repos/mdn/browser-compat-data/contents/webdriver/commands/AcceptAlert.json?ref=master",
-        type: ItemType.Directory,
-        parent: {
-          name: "commands",
-          url:
-            "https://api.github.com/repos/mdn/browser-compat-data/contents/webdriver/commands?ref=master",
-          type: ItemType.Directory,
-          parent: undefined,
-          rootParent: undefined,
-          breadcrumbs: ["webdriver", "commands"]
-        },
-        rootParent: undefined,
-        breadcrumbs: ["webdriver", "commands", "Accept Alert"]
-      };
+      const content: string = mock.parseElementsWithNestingContent;
+      const item: Item = mock.parseElementsWithNestingItem;
 
       const actual = parser.parseElements(content, item);
       const expected: Item[] = [
@@ -261,36 +155,8 @@ describe("Parser", function() {
     });
 
     it("should return array of 1 Item element if content contains object with empty __compat property", function() {
-      content = `
-      {
-        "webdriver": {
-          "commands": {
-            "AcceptAlert": {
-              "__compat": {
-              }
-            }
-          }
-        }
-      }
-      `;
-
-      const item: Item = {
-        name: "Accept Alert",
-        url:
-          "https://api.github.com/repos/mdn/browser-compat-data/contents/webdriver/commands/AcceptAlert.json?ref=master",
-        type: ItemType.Directory,
-        parent: {
-          name: "commands",
-          url:
-            "https://api.github.com/repos/mdn/browser-compat-data/contents/webdriver/commands?ref=master",
-          type: ItemType.Directory,
-          parent: undefined,
-          rootParent: undefined,
-          breadcrumbs: ["webdriver", "commands"]
-        },
-        rootParent: undefined,
-        breadcrumbs: ["webdriver", "commands", "Accept Alert"]
-      };
+      const content: string = mock.parseElementsWithEmptyCompatContent;
+      const item: Item = mock.parseElementsWithEmptyCompatItem;
 
       const actual = parser.parseElements(content, item);
       const expected: Item[] = [
@@ -307,32 +173,8 @@ describe("Parser", function() {
     });
 
     it("should return array of 1 Item element if content doesn't contain any object with __compat property", function() {
-      content = `
-      {
-        "webdriver": {
-          "commands": {
-          }
-        }
-      }
-      `;
-
-      const item: Item = {
-        name: "Accept Alert",
-        url:
-          "https://api.github.com/repos/mdn/browser-compat-data/contents/webdriver/commands/AcceptAlert.json?ref=master",
-        type: ItemType.Directory,
-        parent: {
-          name: "commands",
-          url:
-            "https://api.github.com/repos/mdn/browser-compat-data/contents/webdriver/commands?ref=master",
-          type: ItemType.Directory,
-          parent: undefined,
-          rootParent: undefined,
-          breadcrumbs: ["webdriver", "commands"]
-        },
-        rootParent: undefined,
-        breadcrumbs: ["webdriver", "commands", "Accept Alert"]
-      };
+      const content: string = mock.parseElementsWithNoCompatContent;
+      const item: Item = mock.parseElementsWithNoCompatItem;
 
       const actual = parser.parseElements(content, item);
       const expected: Item[] = [
@@ -349,15 +191,8 @@ describe("Parser", function() {
     });
 
     it("should return empty array if parent element is undefined", function() {
-      const item: Item = {
-        name: "animate Color",
-        url:
-          "https://api.github.com/repos/mdn/browser-compat-data/contents/svg/elements/animateColor.json?ref=master",
-        type: ItemType.Directory,
-        parent: undefined,
-        rootParent: undefined,
-        breadcrumbs: ["svg", "elements", "animate Color"]
-      };
+      const content: string = mock.parseElementsContent;
+      const item: Item = mock.parseElementsItemNoParent;
 
       const actual = parser.parseElements(content, item);
       const expected: Item[] = [];
@@ -374,25 +209,8 @@ describe("Parser", function() {
     });
 
     it("should return array of Item elements if content is array of directories", function() {
-      const item: Item = {
-        name: "api",
-        url: "#",
-        type: ItemType.Directory,
-        breadcrumbs: ["api"]
-      };
-
-      const content = [
-        {
-          url:
-            "https://api.github.com/repos/mdn/browser-compat-data/contents/api/AbortController.json?ref=master",
-          type: ItemType.Directory
-        },
-        {
-          url:
-            "https://api.github.com/repos/mdn/browser-compat-data/contents/api/AbortPaymentEvent.json?ref=master",
-          type: ItemType.Directory
-        }
-      ];
+      const item: Item = mock.parseDirectoriesItem;
+      const content: Array<any> = mock.parseDirectoriesContent;
 
       const actual = parser.parseDirectories(content, item);
       const expected: Item[] = [
