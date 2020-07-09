c3c304e3b534b1024664dd6630d0ea5f693f12a1
Kamil Bysiec <kamil.bysiec.agileplayers@gmail.com>
2020-07-09 21:54:19 +0200

    Rewritten gulp task for fixing coverage report src paths

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
 const gulp = require("gulp");
-const replace = require("gulp-replace");
+const replacer = require("gulp-string-replacer");
 
 function fixCoverageReportSrcPaths() {
+  console.log("*** gulp task called");
   return gulp
     .src("./dist/coverage/coverage-final.json")
     .pipe(
-      replace(
-        `"path":"Users/kamilbysiec/Local Repository/DEVELOPMENT/javascript/vscode-go-to-mdn/`,
-        `"path":"`
-      )
+      replacer([
+        {
+          regex: /(path\":)(.*)(?=src)/,
+          replacement: `path":"`,
+        },
+      ])
     )
     .pipe(gulp.dest("./dist/coverage"));
 }
