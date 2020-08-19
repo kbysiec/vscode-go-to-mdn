4a82b511edd77d396977bc26621ed03bec8ac5f8
Kamil Bysiec <kamil.bysiec.agileplayers@gmail.com>
2020-08-19 21:51:28 +0200

    Refactoring of extensionController tests

--- src/test/tests/extensionController.test.ts
+++ src/test/tests/extensionController.test.ts
@@ -3,31 +3,28 @@ import { assert } from "chai";
 import * as sinon from "sinon";
 import ExtensionController from "../../ExtensionController";
 import { getExtensionContext } from "../util/mockFactory";
+import { restoreStubbedMultiple, stubMultiple } from "../util/stubUtils";
 
 describe("extensionController", () => {
   let context: vscode.ExtensionContext;
   let extensionController: ExtensionController;
   let extensionControllerAny: any;
 
-  before(() => {
+  beforeEach(() => {
     context = getExtensionContext();
     extensionController = new ExtensionController(context);
-  });
-
-  beforeEach(() => {
     extensionControllerAny = extensionController as any;
   });
 
-  afterEach(() => {
-    sinon.restore();
-  });
   describe("browse", () => {
     it("should load data and show quickPick", async () => {
-      const showStub = sinon.stub(extensionControllerAny.quickPick, "show");
-      const loadQuickPickDataStub = sinon.stub(
-        extensionControllerAny.quickPick,
-        "loadQuickPickData"
-      );
+      const [showStub, loadQuickPickDataStub] = stubMultiple([
+        { object: extensionControllerAny.quickPick, method: "show" },
+        {
+          object: extensionControllerAny.quickPick,
+          method: "loadQuickPickData",
+        },
+      ]);
 
       await extensionControllerAny.browse();
       assert.equal(showStub.calledOnce, true);
@@ -37,10 +34,12 @@ describe("extensionController", () => {
 
   describe("clearCache", () => {
     it("should invoke cache clearing function on cache object", async () => {
-      const stub = sinon.stub(extensionControllerAny.cache, "clearCache");
+      const [clearCacheStub] = stubMultiple([
+        { object: extensionControllerAny.cache, method: "clearCache" },
+      ]);
 
       await extensionControllerAny.clearCache();
-      assert.equal(stub.calledOnce, true);
+      assert.equal(clearCacheStub.calledOnce, true);
     });
   });
 });
