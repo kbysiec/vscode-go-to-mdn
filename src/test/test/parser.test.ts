5cb5541dcb63dc4aaaf99c35b199dac3e0820a8d
Kamil Bysiec <kamil.bysiec.agileplayers@gmail.com>
2021-03-03 22:48:25 +0100

    Refactoring: tests setup for parser component extracted to separate file

--- src/test/test/parser.test.ts
+++ src/test/test/parser.test.ts
@@ -1,62 +1,38 @@
 import { assert } from "chai";
 import Parser from "../../parser";
-import Item from "../../interface/Item";
-import ItemType from "../../enum/ItemType";
 import * as mock from "../mock/parser.mock";
+import { getTestSetups } from "../testSetup/parser.testSetup";
 
 describe("Parser", () => {
-  let parser: Parser;
+  let parser: Parser = new Parser();
+  let setups = getTestSetups();
 
-  before(() => {
+  beforeEach(() => {
     parser = new Parser();
+    setups = getTestSetups();
   });
 
   describe("parseFlatElements", () => {
-    it("should return array of Item elements", () => {
+    it("1: should return array of Item elements", () => {
+      const expected = setups.parseFlatElements1();
       const actual = parser.parseFlatElements(mock.flatElementsInput);
-      const expected: Item[] = [
-        {
-          name: "sub-label",
-          url: "#",
-          parent: {
-            name: "elements",
-            url:
-              "https://api.github.com/repos/mdn/browser-compat-data/contents/svg/elements?ref=master",
-            type: ItemType.Directory,
-            parent: undefined,
-            rootParent: undefined,
-            breadcrumbs: ["svg", "elements"],
-          },
-          rootParent: undefined,
-          type: ItemType.File,
-          breadcrumbs: ["api", "test-label", "sub-label"],
-        },
-        {
-          name: "sub-label 2",
-          url: "https://sub-label-2.com",
-          parent: undefined,
-          rootParent: undefined,
-          type: ItemType.File,
-          breadcrumbs: ["api", "test-label", "sub-label 2"],
-        },
-      ];
       assert.deepEqual(actual, expected);
     });
 
-    it("should return empty array", () => {
+    it("2: should return empty array", () => {
       assert.deepEqual(parser.parseFlatElements({}), []);
     });
   });
 
   describe("parseRootDirectories", () => {
-    it("should return array of Item elements once content contains has links", () => {
+    it("1: should return array of Item elements once content contains has links", () => {
       assert.deepEqual(
         parser.parseRootDirectories(mock.rootDirectoriesWithLinks),
         mock.directoriesOutputItems
       );
     });
 
-    it("should return array of Item elements once content doesn't contain any link", () => {
+    it("2: should return array of Item elements once content doesn't contain any link", () => {
       assert.deepEqual(
         parser.parseRootDirectories(mock.rootDirectoriesNoLink),
         []
@@ -65,152 +41,63 @@ describe("Parser", () => {
   });
 
   describe("parseElements", () => {
-    it("should return array of Item elements if content is array of files without unnecessary nesting of keys", () => {
+    it("1: should return array of Item elements if content is array of files without unnecessary nesting of keys", () => {
+      const expected = setups.parseElements1();
       const actual = parser.parseElements(
         mock.parseElementsContent,
         mock.parseElementsItem
       );
-      const expected: Item[] = [
-        {
-          name: "animate Color - reference",
-          url:
-            "https://developer.mozilla.org/docs/Web/SVG/Element/animateColor",
-          type: ItemType.File,
-          parent: mock.parseElementsItem,
-          rootParent: undefined,
-          breadcrumbs: ["svg", "elements", "animate Color", "animate Color"],
-        },
-        {
-          name: "by",
-          url: "",
-          type: ItemType.File,
-          parent: mock.parseElementsItem,
-          rootParent: undefined,
-          breadcrumbs: ["svg", "elements", "animate Color", "by"],
-        },
-      ];
 
       assert.deepEqual(actual, expected);
     });
 
-    it("should return array of Item elements if content is array of files with unnecessary nesting of keys", () => {
+    it("2: should return array of Item elements if content is array of files with unnecessary nesting of keys", () => {
+      const expected = setups.parseElements2();
       const actual = parser.parseElements(
         mock.parseElementsWithNestingContent,
         mock.parseElementsWithNestingItem
       );
-      const expected: Item[] = [
-        {
-          name: "Accept Alert - reference",
-          url:
-            "https://developer.mozilla.org/docs/Web/WebDriver/Commands/AcceptAlert",
-          type: ItemType.File,
-          parent: mock.parseElementsWithNestingItem,
-          rootParent: undefined,
-          breadcrumbs: [
-            "webdriver",
-            "commands",
-            "Accept Alert",
-            "Accept Alert",
-          ],
-        },
-        {
-          name: "wildcard",
-          url: "",
-          type: ItemType.File,
-          parent: mock.parseElementsWithNestingItem,
-          rootParent: undefined,
-          breadcrumbs: ["webdriver", "commands", "Accept Alert", "wildcard"],
-        },
-      ];
 
       assert.deepEqual(actual, expected);
     });
 
-    it("should return array of 1 Item element if content contains object with empty __compat property", () => {
+    it("3: should return array of 1 Item element if content contains object with empty __compat property", () => {
+      const expected = setups.parseElements3();
       const actual = parser.parseElements(
         mock.parseElementsWithEmptyCompatContent,
         mock.parseElementsWithEmptyCompatItem
       );
-      const expected: Item[] = [
-        {
-          name: "Accept Alert - reference",
-          url: "",
-          type: ItemType.File,
-          parent: mock.parseElementsWithEmptyCompatItem,
-          rootParent: undefined,
-          breadcrumbs: [
-            "webdriver",
-            "commands",
-            "Accept Alert",
-            "Accept Alert",
-          ],
-        },
-      ];
 
       assert.deepEqual(actual, expected);
     });
 
-    it("should return array of 1 Item element if content doesn't contain any object with __compat property", () => {
+    it("4: should return array of 1 Item element if content doesn't contain any object with __compat property", () => {
+      const expected = setups.parseElements4();
       const actual = parser.parseElements(
         mock.parseElementsWithNoCompatContent,
         mock.parseElementsWithNoCompatItem
       );
-      const expected: Item[] = [
-        {
-          name: "Accept Alert - reference",
-          url: "",
-          type: ItemType.File,
-          parent: mock.parseElementsWithNoCompatItem,
-          rootParent: undefined,
-          breadcrumbs: [
-            "webdriver",
-            "commands",
-            "Accept Alert",
-            "Accept Alert",
-          ],
-        },
-      ];
 
       assert.deepEqual(actual, expected);
     });
 
-    it("should return empty array if parent element is undefined", () => {
+    it("5: should return empty array if parent element is undefined", () => {
       const actual = parser.parseElements(
         mock.parseElementsContent,
         mock.parseElementsItemNoParent
       );
-      const expected: Item[] = [];
 
-      assert.deepEqual(actual, expected);
+      assert.deepEqual(actual, []);
     });
   });
 
   describe("parseDirectories", () => {
-    it("should return array of Item elements if content is array of directories", () => {
+    it("1: should return array of Item elements if content is array of directories", () => {
+      const expected = setups.parseDirectories1();
       const actual = parser.parseDirectories(
         mock.parseDirectoriesContent,
         mock.parseDirectoriesItem
       );
-      const expected: Item[] = [
-        {
-          name: "Abort Controller",
-          url:
-            "https://api.github.com/repos/mdn/browser-compat-data/contents/api/AbortController.json?ref=master",
-          type: ItemType.Directory,
-          parent: mock.parseDirectoriesItem,
-          rootParent: mock.parseDirectoriesItem,
-          breadcrumbs: ["api", "Abort Controller"],
-        },
-        {
-          name: "Abort Payment Event",
-          url:
-            "https://api.github.com/repos/mdn/browser-compat-data/contents/api/AbortPaymentEvent.json?ref=master",
-          type: ItemType.Directory,
-          parent: mock.parseDirectoriesItem,
-          rootParent: mock.parseDirectoriesItem,
-          breadcrumbs: ["api", "Abort Payment Event"],
-        },
-      ];
 
       assert.deepEqual(actual, expected);
     });
