3796cc43cda38034eec26e09ec16a04162a69626
Kamil Bysiec <kamil.bysiec.agileplayers@gmail.com>
2021-03-20 15:02:20 +0100

    updated url for downloading data for flat view

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -3,7 +3,7 @@ export const appConfig = {
   searchUrl: "https://developer.mozilla.org/en-US/search",
   rootUrl:
     "https://api.github.com/repos/mdn/browser-compat-data/contents/README.md?ref=master",
-  allFilesUrl: "http://api.agileplayers.com/api/mdn-data",
+  allFilesUrl: "http://vscodegotomdnapi.agileplayers.com/get",
   urlNormalizer: {
     from: "https://github.com/",
     to: "https://api.github.com/",
